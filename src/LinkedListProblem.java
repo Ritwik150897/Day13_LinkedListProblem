1f20c21dab33bc9c082e9ced137fcd58bfa5aa1a
Ritwik <deshrutu@gmail.com>
2022-06-10 21:42:42 +0530

    UC8 - Insert a Node After Given Node

--- src/LinkedListProblem.java
+++ src/LinkedListProblem.java
@@ -17,18 +17,22 @@ public class LinkedListProblem {
 
         list.removeLast();
 
-        Iterator<String> itr = list.iterator();
-        while (itr.hasNext()) {
-            System.out.print(" " + itr.next());
-        }
-
         String key = "30";
+        int i;
         System.out.println();
-        for (int i = 0; i < list.size(); i++) {
+        for (i = 0; i < list.size(); i++) {
             String value = list.get(i);
             if (value == key) {
                 System.out.println("Value " + key + " found at Node: " + i);
             }
+        }
+        
+        list.add(i++, "40");
+        list.addLast("70");
+
+        Iterator<String> itr = list.iterator();
+        while (itr.hasNext()) {
+            System.out.print(" " + itr.next());
         }
     }
 }
