368b5e969a7f8862c6fb382d52489df255b06d34
Ritwik <deshrutu@gmail.com>
2022-06-10 20:47:55 +0530

    UC1 - LinkedList Created

--- src/LinkedListProblem.java
+++ src/LinkedListProblem.java
@@ -1,5 +1,9 @@
+import java.util.LinkedList;
+
 public class LinkedListProblem {
     public static void main(String[] args) {
         System.out.println("LinkedList Problem");
+
+        LinkedList<String> list = new LinkedList<String>();
     }
 }
